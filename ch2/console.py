231a266b7be627932ce40510ed3a7b0bca9738b8
Alan <alanland@163.com>
2016-05-08 15:55:24 +0800

    kppp

---
+++ ch2/console.py
@@ -0,0 +1,7 @@
+import kNNN
+
+group, labels = kNNN.create_dataset()
+
+print group
+
+print  labels
---
+++ ch2/kNNN.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from numpy import *
+
+
+def create_dataset():
+    group = array([[1.0, 1.0], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+
+
